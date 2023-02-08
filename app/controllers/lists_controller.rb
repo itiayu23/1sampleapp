703e30523de49e8e8a39133da4853e690a44ca5e
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-02-08 15:36:31 +0000

    App1 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,23 @@
 class ListsController < ApplicationController
   def new
+    # viewへ渡すためのインスタンス変数にからのModelオブジェを生成する。
+    @list = List.new
   end
+end
+def create
+  # 投稿データを受け取り新規登録するためのインスタンス作成
+  list = List.new(list_params)
+  # データベースに保存するためのメソッド実行
+  list.save
+  # top画面にリダイレクト
+  redirect_to '/top'
+end
+
+
+
 
   def index
+    
   end
 
   def show
@@ -10,4 +25,9 @@ class ListsController < ApplicationController
 
   def edit
   end
-end
+
+ private
+# ストロングパラメータ
+ def list_params
+  params.require(:list).permit(:title, :body)
+ end
