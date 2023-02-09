ee97bf4f2509788f5a3ccc85959dc90b4d3f05d1
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-02-09 09:41:54 +0000

    App1 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,14 @@ class ListsController < ApplicationController
 
  def create
   # 投稿データを受け取り新規登録するためのインスタンス作成
-  list = List.new(list_params)
+  @list = List.new(list_params)
   # データベースに保存するためのメソッド実行
-  list.save
+  if @list.save
   # top画面にリダイレクト
-  redirect_to list_path(list.id)
+  redirect_to list_path(@list.id)
+  else
+    render :new
+  end
  end
 
 
@@ -35,12 +38,19 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
   
+  def destroy
+    list = List.find(params[:id]) #データ取得
+    list.destroy #データ削除
+    redirect_to '/lists' #投稿一覧へ
+    
+  end
+  
  private
 # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
- end
+end
  
  
  
